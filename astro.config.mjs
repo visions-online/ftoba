720a0d119c1e3efc26b174767705f5e90da06e8b
b.santee <b.santee@visions-online.com>
2025-02-17 03:30:10 +0000

    name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Fuck the OBA',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
