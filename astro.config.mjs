574091c59b96a2a1413ebbbb9a1ac3e2cc4a7e8a
b.santee <b.santee@visions-online.com>
2025-02-25 03:39:10 +0000

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ottowa.netlify.app',
 	integrations: [
 		starlight({
 			title: 'FUCK the O.B.A.',
@@ -18,5 +20,6 @@ export default defineConfig({
 				}
 			],
 		}),
+		sitemap(),
 	],
 });
